e0d866da9cbc5df7e1b520bfe150860db449fc50
4lbinP <kastanjake@gmail.com>
2025-05-15 16:31:12 +0200

    nice nav bar

---
+++ script/header.js
@@ -0,0 +1,14 @@
+fetch('/templates/header.html')
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('header').innerHTML = data;
+
+        const links = document.querySelectorAll('.nav');
+        const path = window.location.pathname.split("/").pop() || "index.html";
+
+        links.forEach(link => {
+            if (link.getAttribute('href') === path) {
+                link.classList.add('active');
+            }
+        });
+    }).catch(err => console.error('Kunde inte ladda header.html:', err));
