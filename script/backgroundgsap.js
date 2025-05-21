78f9b1fc82f487ff96fb4685fcc8ff5c7414e583
4lbinP <kastanjake@gmail.com>
2025-05-21 23:41:29 +0200

    updated to use gsap for background animation

---
+++ script/backgroundgsap.js
@@ -0,0 +1,29 @@
+
+
+
+let tl = gsap.timeline({
+    repeat: -1,
+})
+
+tl.fromTo("body", 
+    {
+        background: "linear-gradient(135deg, oklch(20.8% 0.042 265.755), oklch(55.4% 0.042 265.755) 100%, oklch(55.4% 0.042 265.755))"
+    },
+    {
+        duration: 5,
+        background: "linear-gradient(135deg, oklch(55.4% 0.042 265.755), oklch(55.4% 0.042 265.755) 0%, oklch(20.8% 0.042 265.755))",
+        ease: "none",
+    })
+.fromTo("body",
+    {
+        background: "linear-gradient(135deg, oklch(55.4% 0.042 265.755), oklch(20.8% 0.042 265.755) 100%, oklch(20.8% 0.042 265.755))",
+    },
+    {
+        duration: 5,
+        background: "linear-gradient(135deg, oklch(20.8% 0.042 265.755), oklch(20.8% 0.042 265.755) 0%, oklch(55.4% 0.042 265.755))",
+        ease: "none",
+    })
+
+document.querySelectorAll(".header a").forEach(link => {
+console.log(link)
+});
